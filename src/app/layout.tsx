b6c59265cbe3129e365ec8e3cc67780eaac629dc
Anastasiya Gurianova <gurianova.nast@gmail.com>
2025-08-18 17:13:23 +0300

    create a layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/UI/header";
 import Providers from "@/providers/Providers";
 import { siteConfig } from "@/config/site.config";
+import { layoutConfig } from "@/config/layout.config";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,8 +32,23 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          <Header />
-          {children}
+          <div className="flex min-h-screen flex-col justify-between">
+            <div className="flex flex-col">
+              <Header />
+              <main
+                className={`flex flex-col max-w-[1024px] mx-auto px-[24px] justify-start items-center`}
+              >
+                {children}
+              </main>
+            </div>
+
+            <footer
+              className={`w-full flex items-center justify-center py-3`}
+              style={{ height: layoutConfig.footerHeight }}
+            >
+              <p>{siteConfig.description}</p>
+            </footer>
+          </div>
         </Providers>
       </body>
     </html>
