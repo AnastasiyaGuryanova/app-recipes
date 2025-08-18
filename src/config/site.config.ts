157c1d98637aa2597b391ff67220675719f912b3
Anastasiya Gurianova <gurianova.nast@gmail.com>
2025-08-18 16:58:58 +0300

    take out the constants

---
+++ src/config/site.config.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  title: "Татарская кухня",
+  description: "Рецепты татарской кухни",
+  navItems: [
+    { href: "/", label: "Рецепты" },
+    { href: "/ingredients", label: "Ингредиенты" },
+    { href: "/about", label: "О нас" },
+  ],
+  pagesContent: {
+    "/": {
+      content: "Здесь будут рецепты...",
+    },
+  },
+};
