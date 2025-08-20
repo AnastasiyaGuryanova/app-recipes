fde372dfbdb6ddcb5d0c24a8869a22b109c7fb57
Anastasiya Gurianova <gurianova.nast@gmail.com>
2025-08-20 06:18:26 +0300

    fix text style in modal

--- src/components/common/modal.tsx
+++ src/components/common/modal.tsx
@@ -16,13 +16,13 @@ const CustomModal = ({
   onClose,
   title,
   children,
-  size = "xs"
+  size = "xs",
 }: IProps) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} size={size}>
       <ModalContent>
         <ModalHeader className="border-b">
-          <h3 className="text-xl text-background font-semibold">{title}</h3>
+          <h3 className="text-xl font-semibold">{title}</h3>
         </ModalHeader>
         <ModalBody className="space-y-4 py-6">{children}</ModalBody>
       </ModalContent>
