16b4fe74a3d690e441d9d3cfc799a701abeadd77
Anastasiya Gurianova <gurianova.nast@gmail.com>
2025-08-25 09:14:48 +0300

    fix zod schema

--- src/schema/zod.ts
+++ src/schema/zod.ts
@@ -4,6 +4,6 @@ export const signInSchema = object({
   email: string().nonempty("Email is required").email("Invalid email"),
   password: string()
     .nonempty("Password is required")
-    .min(8, "Password must be more than 8 characters")
+    .min(6, "Password must be more than 8 characters")
     .max(32, "Password must be less than 32 characters"),
 });
