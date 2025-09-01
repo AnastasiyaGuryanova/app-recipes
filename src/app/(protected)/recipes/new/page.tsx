5d3820137fb1cf13defcf91e4214e35e4802ba1e
Anastasiya Gurianova <gurianova.nast@gmail.com>
2025-09-01 07:30:01 +0300

    fix centering of heading in NewRecipePage

--- src/app/(protected)/recipes/new/page.tsx
+++ src/app/(protected)/recipes/new/page.tsx
@@ -5,7 +5,9 @@ import RecipeForm from "@/forms/recipe.form";
 export default function NewRecipePage() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">Создать новый рецепт</h1>
+      <h1 className="text-3xl flex justify-center font-bold mb-4">
+        Создать новый рецепт
+      </h1>
       <RecipeForm />
     </div>
   );
